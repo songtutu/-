b9ccfbebf5fec9179e4872cada6691cd23cea26f
songtutu <1172120378@qq.com>
2020-04-02 08:59:29 +0800

    tagbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Profile = () => import('views/profile/Profile')
+const Cart = () => import('views/cart/Cart')
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/category',
+      component: Category
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/cart',
+      component: Cart
     }
-  ]
+
+  ],
+  mode: 'history'
 })
